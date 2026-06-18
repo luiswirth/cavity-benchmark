5a36729f84b4392ed14bbb961c2c22ef3ce56471
Luis Wirth <lwirth2000@gmail.com>
2026-06-18 19:27:14 +0200

    rsync --mkpath; fix tilde expansion in REMOTE

--- pull-euler.sh
+++ pull-euler.sh
@@ -3,13 +3,13 @@
 # Run from the cavity-benchmark root.
 set -euo pipefail
 EULER=euler
-REMOTE=$EULER:~/semproj
+REMOTE="$EULER"':~/semproj'
 
-rsync -av --exclude=work/ --exclude=logs/ "$REMOTE/cavity-bem/out/grid/ellipse/"  out/bem/grid/ellipse/
-rsync -av --exclude=work/ --exclude=logs/ "$REMOTE/cavity-bem/out/grid/sphere/"   out/bem/grid/sphere/
-rsync -av --exclude=work/ --exclude=logs/ "$REMOTE/cavity-bem/out/ref/ellipse/"   out/bem/ref/ellipse/
-rsync -av --exclude=work/ --exclude=logs/ "$REMOTE/cavity-bem/out/ref/sphere/"    out/bem/ref/sphere/
-rsync -av --exclude=work/ --exclude=logs/ "$REMOTE/cavity-epgp/out/grid/ellipse/" out/epgp/grid/ellipse/
-rsync -av --exclude=work/ --exclude=logs/ "$REMOTE/cavity-epgp/out/grid/sphere/"  out/epgp/grid/sphere/
-rsync -av --exclude=work/ --exclude=logs/ "$REMOTE/cavity-epgp/out/ref/ellipse/"  out/epgp/ref/ellipse/
-rsync -av --exclude=work/ --exclude=logs/ "$REMOTE/cavity-epgp/out/ref/sphere/"   out/epgp/ref/sphere/
+rsync -av --mkpath --exclude=work/ --exclude=logs/ "$REMOTE/cavity-bem/out/grid/ellipse/"  out/bem/grid/ellipse/
+rsync -av --mkpath --exclude=work/ --exclude=logs/ "$REMOTE/cavity-bem/out/grid/sphere/"   out/bem/grid/sphere/
+rsync -av --mkpath --exclude=work/ --exclude=logs/ "$REMOTE/cavity-bem/out/ref/ellipse/"   out/bem/ref/ellipse/
+rsync -av --mkpath --exclude=work/ --exclude=logs/ "$REMOTE/cavity-bem/out/ref/sphere/"    out/bem/ref/sphere/
+rsync -av --mkpath --exclude=work/ --exclude=logs/ "$REMOTE/cavity-epgp/out/grid/ellipse/" out/epgp/grid/ellipse/
+rsync -av --mkpath --exclude=work/ --exclude=logs/ "$REMOTE/cavity-epgp/out/grid/sphere/"  out/epgp/grid/sphere/
+rsync -av --mkpath --exclude=work/ --exclude=logs/ "$REMOTE/cavity-epgp/out/ref/ellipse/"  out/epgp/ref/ellipse/
+rsync -av --mkpath --exclude=work/ --exclude=logs/ "$REMOTE/cavity-epgp/out/ref/sphere/"   out/epgp/ref/sphere/
